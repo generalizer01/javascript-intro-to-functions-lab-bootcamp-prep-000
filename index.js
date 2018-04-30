54fb616c6a9afff8ac4d0efc13227b1bc8fbb7c5
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-04-30 18:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return 'I love you, too'
   }
   else if (string.toUpperCase()===string) {
-    return 'YES INDEED'
+    return 'YES INDEED!'
   }
   else {
     return "I can't hear you!"
